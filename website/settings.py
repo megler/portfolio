1cdd72e3e8fa434bf8339836493fb12c2ab1676f
megler <majeresmom@att.net>
2022-05-20 15:08:05 -0400

    reconfigured installed_apps in settings

--- website/settings.py
+++ website/settings.py
@@ -40,16 +40,16 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.auth",
+    "portfolio",
+    "movies",
+    "twitter",
     "django.contrib.admin",
+    "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "captcha",
-    "portfolio",
-    "movies",
-    "twitter",
 ]
 
 MIDDLEWARE = [
