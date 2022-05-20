c72fa80e4f2c8e16093d05a60fed4945c890c891
megler <majeresmom@att.net>
2022-05-20 14:49:36 -0400

    updated allowed hosts

--- website/settings.py
+++ website/settings.py
@@ -31,7 +31,11 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "https://megler-portfolio.herokuapp.com/",
+    "127.0.0.1",
+    "localhost",
+]
 
 # Application definition
 
