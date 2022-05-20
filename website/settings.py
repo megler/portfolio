713d3793155ed8406291ce5c7c62f4fe6c3916be
megler <majeresmom@att.net>
2022-05-20 15:26:10 -0400

    debug on

--- website/settings.py
+++ website/settings.py
@@ -29,7 +29,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "https://megler-portfolio.herokuapp.com/",
