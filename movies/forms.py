201ddcfaa9ce63e92b02cfbcf2d711d0dc8e46c8
megler <majeresmom@att.net>
2022-05-24 10:20:01 -0400

    movies now rate 1-10

--- movies/forms.py
+++ movies/forms.py
@@ -8,6 +8,11 @@ class Movie_Title(forms.Form):
 
 class Movie_Rating(forms.Form):
     movie_rating = forms.FloatField(widget=forms.NumberInput(
-        attrs={"class": "form-control"}))
+        attrs={
+            "class": "form-control",
+            "min": 0,
+            "max": 10,
+            "type": "number"
+        }))
     movie_review = forms.CharField(widget=forms.Textarea(
         attrs={"class": "form-control"}))
